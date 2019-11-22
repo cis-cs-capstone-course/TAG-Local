dd8990336c31546c444eaa9bcca849f087470d2c
Leo Vergnetti <tuj52136@temple.edu>
2019-11-21 23:37:57 -0500

    Add train.py and annotate.py scripts to project

---
+++ py/annotate.py
@@ -0,0 +1,58 @@
+import json
+import os
+import argparse
+import sys
+import random
+from pathlib import Path
+import spacy
+import re
+import string
+from spacy.util import minibatch, compounding
+
+class documentClass:
+    def __init__(self, title, text, annotations):
+        self.title = title
+        self.text = text
+        self.annotations = annotations
+
+class annotationClass:
+    def __init__(self, label, start, end, content):
+        self.range = {'startPosition': start, 'endPosition': end}
+        self.content = content
+        self.label = label
+
+
+def main(model, raw_data):
+    data = json.loads(raw_data)
+    nlp = spacy.load(model)
+    print("Loaded model '%s'" % model)
+    docs = []
+    for d in data:
+        doc = nlp(d['text'])
+        returnData = []
+        for ent in doc.ents:
+            annotation = annotationClass(ent.label_, ent.start_char, ent.end_char, ent.text)
+            print("Found entity: %s in %s" % (ent.text, d['title']))
+            sys.stdout.flush()
+            returnData.append(annotation.__dict__)
+        docs.append(documentClass(d['title'], d['text'], returnData).__dict__)
+        # print("Found %d entities", doc.ents.count)
+    return json.dumps(docs)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--model_path',
+        help="path to ML model"
+    )
+    parser.add_argument(
+        '--data_path',
+        help="Path to the data directory."
+    )
+
+
+args = parser.parse_args()
+print("args parsed")
+print(args)
+sys.stdout.flush()
+main(args.model_path, args.data_path)
