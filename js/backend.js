ac3fef336d66e1ce452593b55d0cc13d930e9652
Leo Vergnetti <tuj52136@temple.edu>
2019-11-30 23:49:55 -0500

    add training to menubar, refactor training method from click handler

--- js/backend.js
+++ js/backend.js
@@ -12,37 +12,33 @@ let annotateScript = path.join(__dirname, 'py', 'annotate.py');
 let annotateScriptLocal = path.join(__dirname, 'py', 'annotate.py');
 
 // file path picker is open
-var dialogOpen = false;
+// var dialogOpen = false;
 // hide things supposed to be hidden
 $('#trainAll').hide();
 $('#trainThis').hide();
 $('#modelPath').hide();
 
 // add new model path
-$('#loadNew').on('click', function () {
-    // check file path is open
-    if (!dialogOpen) {
-        // is not open // open
-        console.log("Opening directory picker");
-        dialogOpen = true;
-        dialog.showOpenDialog({
-            title: "Select a folder",
-            properties: ["openDirectory"]
-        }).then(function (data) {
+$('#loadNew').on('click', loadExistingML);
+// });
+
+function loadExistingML(){
+  console.log("Opening directory picker");
+  dialog.showOpenDialog(remote.getCurrentWindow(), {
+      title: "Select a folder",
+      properties: ["openDirectory"]
+  }).then(function (data) {
             // on close get file path
-            if (data.filePaths[0]) {
-                console.log("Changing paths to: '" + data.filePaths[0] + "'");
-                tagModel.currentModel = data.filePaths[0].goodPath();
-                $('#trainAll').show();
-                $('#trainThis').show();
-                $('#modelPath').text(tagModel.currentModel.truncStart(30, true)).show();
-            }
-            dialogOpen = false;
-        });
-    } else {
-        console.log("Directory picker already open");
-    }
-});
+      if (data.filePaths[0]) {
+        console.log("Changing paths to: '" + data.filePaths[0] + "'");
+        tagModel.currentModel = data.filePaths[0].goodPath();
+        $('#trainAll').show();
+        $('#trainThis').show();
+        $('#modelPath').text(tagModel.currentModel.truncStart(30, true)).show();
+      }
+  });
+}
+
 
 // try training
 $('.trainButton').on('click', function () {
@@ -52,47 +48,90 @@ $('.trainButton').on('click', function () {
   var isAllDocuments;
 
   if (this.id === "trainAll" || this.id === "trainThis") {
+  //   if (tagModel.currentDoc == null) {
+  //     alert("Please upload a document first!");
+  //     return;
+  //   }
+
+    if (this.id === "trainAll") {
+      console.log("Training all documents...");
+      trainAllDocuments();
+    }
+    else {
+      console.log("Training current document: \"" + tagModel.currentDoc.title + "\"");
+      trainCurrentDocument();
+    }
+    // beginTraining(isAllDocuments);
+  }
+  //
+  // // TODO: replace options
+  // var options = {
+  //     args: ['--raw_data', tagModel.jsonifyData(isAllDocuments), '--n_iter', 30, '--model', tagModel.currentModel]
+  // };
+  // // try app
+  // // TODO: replace with annotate all
+  // let pyReturn;
+  // // try installer path
+  // launchPy(trainScript, options).then(function (data) {
+  //     pyReturn = data;
+  //     alert('!');
+  //     next();
+  // }).catch(function () {
+  //     //try compiled path
+  //     launchPy(trainScriptLocal, options).then(function (data) {
+  //         pyReturn = data;
+  //         alert('!!!');
+  //         next();
+  //     }).catch(function () {
+  //         // still didn't work
+  //         if (!pyReturn) {
+  //             alert('Something went wrong');
+  //             return -1;
+  //         }
+  //     });
+  // });
+});
+
+function trainAllDocuments(){
+  beginTraining(isAllDocuments = true);
+}
+function trainCurrentDocument(){
+  beginTraining(isAllDocuments = false);
+}
+
+function beginTraining(isAllDocuments){
     if (tagModel.currentDoc == null) {
       alert("Please upload a document first!");
       return;
     }
-    if (this.id === "trainAll") {
-      console.log("Annotating all documents...");
-      isAllDocuments = true;
-    }
-    else {
-      console.log("Annotating document: \"" + tagModel.currentDoc.title + "\"");
-      isAllDocuments = false;
-    }
-  }
-
-  // TODO: replace options
-  var options = {
-      args: ['--raw_data', tagModel.jsonifyData(isAllDocuments), '--n_iter', 30, '--model', tagModel.currentModel]
-  };
-  // try app
-  // TODO: replace with annotate all
-  let pyReturn;
-  // try installer path
-  launchPy(trainScript, options).then(function (data) {
-      pyReturn = data;
-      alert('!');
-      next();
-  }).catch(function () {
-      //try compiled path
-      launchPy(trainScriptLocal, options).then(function (data) {
-          pyReturn = data;
-          alert('!!!');
-          next();
-      }).catch(function () {
-          // still didn't work
-          if (!pyReturn) {
-              alert('Something went wrong');
-              return -1;
-          }
-      });
-  });
-});
+    // TODO: replace options
+    var options = {
+        args: ['--raw_data', tagModel.jsonifyData(isAllDocuments), '--n_iter', 30, '--model', tagModel.currentModel]
+    };
+    // try app
+    // TODO: replace with annotate all
+    let pyReturn;
+    // try installer path
+    launchPy(trainScript, options).then(function (data) {
+        pyReturn = data;
+        alert('Training Complete!');
+        //falling through
+        next();
+    }).catch(function () {
+        //try compiled path
+        launchPy(trainScriptLocal, options).then(function (data) {
+            pyReturn = data;
+            alert('Training Complete (local script)!');
+            next();
+        }).catch(function () {
+            // still didn't work
+            if (!pyReturn) {
+                alert('Something went wrong');
+                return -1;
+            }
+        });
+    });
+}
 
 $('.annButton').on('click', function () {
   var isAllDocuments;
